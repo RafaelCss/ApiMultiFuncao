0ad104e7053bfdb6c2e5f642cb6ef0f912e014c5
devspacerj <rf2014souza@gmail.com>
2022-10-23 22:37:59 -0300

    23/10/22 atualizando codigo

--- Modelo.Dominio/Entidade/Usuario.cs
+++ Modelo.Dominio/Entidade/Usuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Flunt.Notifications;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,29 @@ using System.Threading.Tasks;
 
 namespace Modelo.Dominio.Entidade
 {
-	public class Usuario 
-	{	
-		public string Id { get; set; }
-		public string Nome { get; set; }
-		public string Email { get; set; }
-		public string Telefone { get; set; }
-		public string Senha { get; set; }
+	public class Usuario : Notifiable<Notification>
+	{
+
+		public Usuario(string nome, string email, string telefone, string senha )
+		{
+			ValidarNome(nome);
+		}
+
+
+		public string Id { get; private set; }
+		public string Nome { get; private set; }
+		public string Email { get; private set; }
+		public string Telefone { get; private set; }
+		public string Senha { get;	private set; }
+
+		public string ValidarNome(string nome)
+		{
+			if(nome.Length > 0) Nome = nome;
+
+			AddNotifications(Nome, "Este campo não pode ficar vazio");
+
+			return nome;
+		}
+			 
 	}
 }
---
+++ Modelo.Dominio/Notificacao/Validacao.cs
@@ -0,0 +1,32 @@
+﻿using Flunt.Notifications;
+using Modelo.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Dominio.Validacao
+{
+	public class CustomNotification : Notification
+	{
+		public CustomNotification(string key,string message)
+		{
+			Key = key;
+			Message = message;
+		}
+
+		public class CustomNotifiable : Notifiable<CustomNotification>, INotifiable
+		{
+			public void AddNotifications(IEnumerable<Notification> notifications)
+			{
+				foreach(var item in notifications)
+				{
+					var notification = new CustomNotification(item.Key,item.Message);
+					AddNotification(notification);
+				}
+			}
+		}
+
+	}
+}
